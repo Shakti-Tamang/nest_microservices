4d1163d723ba1e33c4ffd4a564b22fb3b9973d39
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-12-15 15:02:36 +0545

    fix : fix prittier issue

--- apps/logging-service/src/main.ts
+++ apps/logging-service/src/main.ts
@@ -3,6 +3,9 @@ import { LoggingServiceModule } from './logging-service.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(LoggingServiceModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.port ?? 3001);
 }
 bootstrap();
+
+
+// nest generate app shakti-service
